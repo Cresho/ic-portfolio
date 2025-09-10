c7270a22fefc5dd86bbc17bb7af91b40af3d583c
Krešimir Matić <73597638+Cresho@users.noreply.github.com>
2025-09-10 13:17:13 +0200

    Navbar complete

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,19 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import LogoSub from '../../assets/images/logo-i-sub.png';
+
 
 const Navbar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoSub} alt="logo" />
         </Link>
+
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">Work</NavLink>
+            <NavLink exact="true" activeclassname="active" to="/about">About</NavLink>
+            <NavLink exact="true" activeclassname="active" to="/contact">Contact</NavLink>
+        </nav>
     </div>
 );
 
