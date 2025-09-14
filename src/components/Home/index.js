0d871dba8cc3ea1feec3f19d54f57adf547ef534
Krešimir Matić <73597638+Cresho@users.noreply.github.com>
2025-09-14 12:48:13 +0200

    Homepage structure done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,18 @@
 import './index.scss';
+import placeholderImage from '../../assets/images/placeholder.jpg';
 
 const Home = () => {
+
+    const images = [
+        { src: placeholderImage, link: 'https://www.youtube.com/watch?v=5AtvXdmPe3A&list=RD5AtvXdmPe3A&start_radio=1&ab_channel=JohnnyCash-Topic'},
+        { src: placeholderImage, link: 'https://www.youtube.com/watch?v=5AtvXdmPe3A&list=RD5AtvXdmPe3A&start_radio=1&ab_channel=JohnnyCash-Topic'},
+        { src: placeholderImage, link: 'https://www.youtube.com/watch?v=5AtvXdmPe3A&list=RD5AtvXdmPe3A&start_radio=1&ab_channel=JohnnyCash-Topic'},
+        { src: placeholderImage, link: 'https://www.youtube.com/watch?v=5AtvXdmPe3A&list=RD5AtvXdmPe3A&start_radio=1&ab_channel=JohnnyCash-Topic'},
+        { src: placeholderImage, link: 'https://www.youtube.com/watch?v=5AtvXdmPe3A&list=RD5AtvXdmPe3A&start_radio=1&ab_channel=JohnnyCash-Topic'},
+        { src: placeholderImage, link: 'https://www.youtube.com/watch?v=5AtvXdmPe3A&list=RD5AtvXdmPe3A&start_radio=1&ab_channel=JohnnyCash-Topic'},
+    ];
+
+
     return (
         <div className="container home-page">
             <div className="text-zone">
@@ -8,6 +20,23 @@ const Home = () => {
                     I use concept development, visual communication and user-centered design in Adobe Creative Suite to create awesome digital experiences.
                 </h1>
             </div>
+            <div className="image-zone">
+                {images.map((image, index) => (
+                    <a key={index} href={image.link} target="_blank" rel="noopener noreferrer">
+                        <img src={image.src} alt={`Image ${index + 1}`} />
+                    </a>
+                ))}
+            </div>
+
+            <div className="bottom-zone">
+                <h1>Ivana Čalić</h1>
+                <h1>ivana.calic01@gmail.com</h1>
+                <h1><a href="https://www.linkedin.com/in/ivana-calic/">LinkedIn</a></h1>
+            </div>
+
+            <footer>
+                <p>© 2024 Ivana Čalić. All rights reserved.</p>
+            </footer>
         </div>
     )
 }
