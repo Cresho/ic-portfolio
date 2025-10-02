b5f8543a1dc51cfbbbab53a40e069baf1ccaff41
Krešimir Matić <73597638+Cresho@users.noreply.github.com>
2025-10-02 02:11:22 +0200

    HashRouter deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -15,22 +15,19 @@ import Trustiva from './components/projects/Trustiva';
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="policyhounds" element={<PolicyHounds />} />
-            <Route path="susanyap" element={<SusanYap />} />
-            <Route path="charliehealthcare" element={<CharlieHealthcare />} />
-            <Route path="craftmapped" element={<CraftMapped />} />
-            <Route path="glidemedicare" element={<GlideMedicare />} />
-            <Route path="trustiva" element={<Trustiva />} />
-          </Route>
-        </Routes>
-      </Router>
-      
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="policyhounds" element={<PolicyHounds />} />
+          <Route path="susanyap" element={<SusanYap />} />
+          <Route path="charliehealthcare" element={<CharlieHealthcare />} />
+          <Route path="craftmapped" element={<CraftMapped />} />
+          <Route path="glidemedicare" element={<GlideMedicare />} />
+          <Route path="trustiva" element={<Trustiva />} />
+        </Route>
+      </Routes>
     </>
   )
 }
